fe0086a38c4da9030b7f399f71cb1ba3ab1aa211
HussainGafrony <hgafrony@gmail.com>
2023-05-14 05:17:13 +0300

    add middleware

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,10 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use App\Http\Controllers\AddressController;
-use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,36 +16,38 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+ */
 //USER
 Route::get('user', [UserController::class, 'index']);
 Route::post('user', [UserController::class, 'store']);
-Route::post('login', [UserController::class, 'login']);
-Route::post('update', [UserController::class, 'update']);
-//Address
-Route::post('address', [AddressController::class, 'index']);
-Route::post('update', [AddressController::class, 'update']);
-Route::post('store', [AddressController::class, 'store']);
-
 //Products
 Route::get('product', [ProductsController::class, 'index']);
-Route::post('productS', [ProductsController::class, 'store']);
-Route::post('productU', [ProductsController::class, 'update']);
-Route::post('product', [ProductsController::class, 'destroy']);
-Route::get('search/{word}', [ProductsController::class, 'search']);
 //Category
 Route::get('category', [CategoryController::class, 'index']);
-Route::post('categoryS', [CategoryController::class, 'store']);
-Route::post('categoryU', [CategoryController::class, 'update']);
-Route::post('category', [CategoryController::class, 'destroy']);
-//Order
-Route::post('order', [OrderController::class, 'index']);
-Route::post('orderS', [OrderController::class, 'store']);
-Route::post('orderU', [OrderController::class, 'update']);
-Route::post('orderD', [OrderController::class, 'destroy']);
-Route::post('delete_orderDetails', [OrderController::class, 'delete_OrderDetailsID']);
 
+Route::middleware('auth:api')->group(function () {
+
+    //USER
+    Route::post('login', [UserController::class, 'login']);
+    Route::post('update', [UserController::class, 'update']);
+    //Address
+    Route::post('address', [AddressController::class, 'index']);
+    Route::post('update', [AddressController::class, 'update']);
+    Route::post('store', [AddressController::class, 'store']);
+    //Products
+    Route::post('productS', [ProductsController::class, 'store']);
+    Route::post('productU', [ProductsController::class, 'update']);
+    Route::post('product', [ProductsController::class, 'destroy']);
+    Route::get('search/{word}', [ProductsController::class, 'search']);
+    //Category
+    Route::post('categoryS', [CategoryController::class, 'store']);
+    Route::post('categoryU', [CategoryController::class, 'update']);
+    Route::post('category', [CategoryController::class, 'destroy']);
+    //Order
+    Route::post('order', [OrderController::class, 'index']);
+    Route::post('orderS', [OrderController::class, 'store']);
+    Route::post('orderU', [OrderController::class, 'update']);
+    Route::post('orderD', [OrderController::class, 'destroy']);
+    Route::post('delete_orderDetails', [OrderController::class, 'delete_OrderDetailsID']);
+
+});
